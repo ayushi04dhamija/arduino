18a4ebd934f942443b61372838d8110d8ed627c1
ayushicoder <31570086+ayushicoder@users.noreply.github.com>
2018-01-26 10:48:01 +0530

    Add files via upload

---
+++ _0to1024.ino
@@ -0,0 +1,20 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(A0, INPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  int a=analogRead(A0);
+  Serial.println(a*0.0358);
+ // analogWrite(3,a);
+ 
+  delay(100);
+/*for(int i=0; i<=1024; i++)
+{ 
+  analogWrite(3,i);
+  delay(100);
+  
+}
+*/
+}
---
+++ read.ino
@@ -0,0 +1,15 @@
+void setup() {
+  Serial.begin(9600);
+  
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+if(Serial.available()>0)
+{
+char ch = Serial.read();
+Serial.println(ch);
+}
+}
